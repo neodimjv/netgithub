8569a9e715414e6eae94ae39f6f883c8042fb9e4
Nedim Grmz <neodimjv@gmail.com>
2021-12-03 22:08:44 +0300

    ders2

--- src/netgithub1/ders1.java
+++ src/netgithub1/ders1.java
@@ -4,6 +4,21 @@ public class ders1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+String ogrenciIsim = "Ekrem";
+		
+		System.out.println(ogrenciIsim);
+		
+		int sayi;
+		sayi=10;
+				
+		System.out.println(sayi);
+		
+		sayi=25;
+		System.out.println(sayi);
+		
+		ogrenciIsim="Mustafa";
+		
+		System.out.println(ogrenciIsim);
 
 	}
 
