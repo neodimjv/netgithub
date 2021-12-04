6c212092c90f8e92a178f4a69d208f27d48e3f01
Nedim Grmz <neodimjv@gmail.com>
2021-12-04 22:39:24 +0300

    ders3

---
+++ src/netgithub1/ders2.java
@@ -0,0 +1,30 @@
+package netgithub1;
+
+import java.util.Scanner;
+
+public class ders2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Lütfen bir sayý giriniz");
+		Scanner scan=new Scanner(System.in);
+		int sayýlar=scan.nextInt();
+		int sayýlar2=scan.nextInt();
+		
+		topla(sayýlar+sayýlar2);
+
+	}
+
+	private static void topla(int...sayýlar) {
+		int toplam=0;
+		for (int each:sayýlar) {
+			toplam+=each;
+			
+		}
+		
+		
+		System.out.println("Girilen sayýlarýn Toplamý :" +toplam);
+
+	}
+
+}
---
+++ src/netgithub1/ders2_delete.java
@@ -0,0 +1,13 @@
+package netgithub1;
+
+public class ders2_delete {
+
+	public static void main(String[] args) {
+StringBuilder sb=new StringBuilder("Java candýr can");
+		
+		sb.delete(11, 19);
+		System.out.println(sb);
+		
+	}
+
+}
