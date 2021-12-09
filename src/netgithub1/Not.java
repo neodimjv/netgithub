f1fff99746d10708b81ff7ac8faac74b8832c915
Nedim Grmz <neodimjv@gmail.com>
2021-12-09 21:04:36 +0300

    not

---
+++ src/netgithub1/Not.java
@@ -0,0 +1,22 @@
+package netgithub1;
+
+import java.util.Scanner;
+
+public class Not {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Bir sayý giriniz");
+		int not=scan.nextInt();
+		
+		switch (not) {
+		case 10: System.out.println("iki basamaklý en küçük sayý");			break;
+		case 100: System.out.println("3 basamaklý en küçük sayý");			break;
+		case 1000: System.out.println("4 basamaklý en küçük sayý");			break;
+		default: System.out.println("Ýþine bak");
+			break;
+		}
+
+	}
+
+}
