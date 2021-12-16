61e328c44a85da879a9a9740696adac4ea255524
Nedim Grmz <neodimjv@gmail.com>
2021-12-16 21:33:31 +0300

    3

--- src/netgithub1/Replace.java
+++ src/netgithub1/Replace.java
@@ -5,6 +5,7 @@ public class Replace {
 	public static void main(String[] args) {
 		String cumle="Biz güzel bir Aileyiz";
 		System.out.println(cumle);
+		System.out.println();
 		System.out.println(cumle.replace("i", "xxx"));
 		System.out.println(cumle.replaceAll(cumle, "hayat çok güzel"));
 
