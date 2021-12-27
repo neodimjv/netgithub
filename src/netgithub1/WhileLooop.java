da898058f3c45c03f0d04fbf3369d124b4d956fb
Nedim Grmz <neodimjv@gmail.com>
2021-12-27 22:01:57 +0300

    while

---
+++ src/netgithub1/WhileLooop.java
@@ -0,0 +1,23 @@
+package netgithub1;
+
+public class WhileLooop {
+
+	public static void main(String[] args) {
+		int a=1;
+		int sayac=0;
+		int toplam=0; 
+		while (a<20) {
+			a+=2;
+			sayac++;
+			toplam+=a;
+			System.out.print(a+" ");
+			System.out.print("");
+			System.out.print(sayac+ "*");
+			System.out.print("");
+			System.out.print(toplam+" -");
+		}
+		
+
+	}
+
+}
